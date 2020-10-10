2a609328d5da92888d78c4dc58db72db6fd72c34
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-10 19:41:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,10 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart.each do |cart_item|   
+        resp.write "#{cart_item}\n"
+      end
     else
       resp.write "Path Not Found"
     end
